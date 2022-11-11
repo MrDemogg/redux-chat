96c679338b64381a2d4c75f6c75aebef4644b79e
demog <dg08gg@mail.ru>
2022-11-11 15:30:01 +0300

    fsHandler.js / Работа с файлами
    Добавил success response, чтобы можно было понять что все прошло успешно, и сменил uuid от uuidv4 На uuidv4 от uuid (запутано как-то)

--- server/fsHandler.js
+++ server/fsHandler.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { uuidv } = require('uuidv4')
+const { v4: uuidv4 } = require('uuid');
 
 const fsHandler = {
   error: {
@@ -7,12 +7,13 @@ const fsHandler = {
     message: null
   },
   postMessage: async (request, response) => {
-    let uniqueId = uuidv()
+    let uniqueId = uuidv4()
+    let error = false
     if (request.author && request.message) {
-      if (request.author.length > 0 && request.message.length > 0) {
+      if (request.author.length !== 0 && request.message !== 0) {
         try {
           while (fs.existsSync(`./server/messages/${uniqueId}.json`)) {
-            uniqueId = uuidv()
+            uniqueId = uuidv4()
           }
           const writeData = {
             id: uniqueId,
@@ -20,16 +21,22 @@ const fsHandler = {
             author: request.author,
             datetime: new Date().toISOString()
           }
-          fs.writeFileSync(`./server/messages/${id}.json`, JSON.stringify(writeData))
+          fs.writeFileSync(`./server/messages/${uniqueId}.json`, JSON.stringify(writeData))
+          response.status(200).send('success')
         } catch (e) {
           fsHandler.error.message = e.message
           response.status(500).send(fsHandler.error)
         }
       } else {
-        fsHandler.error.errorGuilt = 'user'
-        fsHandler.error.message = 'Вы не указали автора, либо отправили пустое сообщение'
-        response.status(400).send(fsHandler.error)
+        error = true
       }
+    } else {
+      error = true
+    }
+    if (error) {
+      fsHandler.error.errorGuilt = 'user'
+      fsHandler.error.message = 'Вы не указали автора, либо отправили пустое сообщение'
+      response.status(400).send(fsHandler.error)
     }
   }
 }
