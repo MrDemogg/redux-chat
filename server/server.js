b61389a43349c34054c94ed044f40c1df8418350
demog <dg08gg@mail.ru>
2022-11-11 15:27:43 +0300

    server.js / Корень сервера
    Покдлючил router для обработки запросов

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const router = require('./router')
 
 const port = 8000
 
 app.use(cors())
 app.use(express.json())
+app.use('', router)
 
 app.listen(port, () => {
   console.log('The server was started on port: ' + port)
