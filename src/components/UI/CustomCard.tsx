6bec0fb52a2ed8dee93729381ffcac67c2990340
demog <dg08gg@mail.ru>
2022-11-12 14:05:16 +0300

    CustomCard.tsx
    Вынес часть с карточкой в отдельный UI компонент, потому что эта часть единтично повторяется в нескольких компонентов и я решил вынести её отдельно

---
+++ src/components/UI/CustomCard.tsx
@@ -0,0 +1,20 @@
+import React, {FC, ReactNode} from 'react';
+import {Card} from "react-bootstrap";
+
+interface CustomCardProps {
+  header: string,
+  body: string,
+  footer: ReactNode
+}
+
+const CustomCard: FC<CustomCardProps> = ({header, body, footer}) => {
+  return (
+    <Card style={{maxWidth: '70%'}}>
+      <Card.Header><Card.Title>{header}</Card.Title></Card.Header>
+      <Card.Body><Card.Text>{body}</Card.Text></Card.Body>
+      <Card.Footer>{footer}</Card.Footer>
+    </Card>
+  );
+};
+
+export default CustomCard;
