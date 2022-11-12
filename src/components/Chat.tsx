59bfa220d685deea6794f9df100266c0f926f473
demog <dg08gg@mail.ru>
2022-11-12 19:51:05 +0300

    Chat.tsx
    Перенёс DateCard в Messages.tsx

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Card} from "react-bootstrap";
 import Messages from "./Messages";
-import DateCard from "./DateCard";
 
 const Chat = () => {
   return (
     <Card>
-      <Card.Body style={{height: '70%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', margin: '0 auto'}}>
+      <Card.Body>
         <Messages/>
-        <DateCard/>
       </Card.Body>
     </Card>
   );
