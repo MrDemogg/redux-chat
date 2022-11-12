481ca6b49df92d5524e40212105fdf892a13c618
demog <dg08gg@mail.ru>
2022-11-12 18:11:58 +0300

    index.tsx
    На всякий получил шрифты для mui

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from './App';
 import {Provider} from "react-redux";
 import {setupStore} from "./store/store";
 import 'bootstrap/dist/css/bootstrap.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
