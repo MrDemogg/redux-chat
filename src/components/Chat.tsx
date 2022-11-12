abc92495b32c3b9a58bb3a823bc22ee685d0154d
demog <dg08gg@mail.ru>
2022-11-12 18:21:50 +0300

    Chat.tsx
    Изменил немного стили

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Card} from "react-bootstrap";
 import Messages from "./Messages";
+import DateCard from "./DateCard";
 
 const Chat = () => {
   return (
     <Card>
-      <Card.Body style={{maxHeight: '70%'}}>
+      <Card.Body style={{height: '70%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', margin: '0 auto'}}>
         <Messages/>
+        <DateCard/>
       </Card.Body>
     </Card>
   );
