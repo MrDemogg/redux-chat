e37e0675be74ec9725dea5da63c006bfa272934d
demog <dg08gg@mail.ru>
2022-11-13 16:22:34 +0300

    DateCard.tsx
    Т.к. в обычном DateTimePicker не имеет выбора секунд, разделил этот Picker на 2 отдельных - DatePicker и TimePicker. Изменил стили. Для разделённого DateTimePicker на 2 части, сделал 2 разных date и time созданные через react'овский useState

--- src/components/DateCard.tsx
+++ src/components/DateCard.tsx
@@ -2,42 +2,61 @@ import React, {useState} from 'react';
 import {Button, Card} from "react-bootstrap";
 import {useAppDispatch} from "../hooks/redux";
 import {chatSlice} from "../store/reducers/ChatSlice";
-import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import {DatePicker, LocalizationProvider, TimePicker} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import {TextField} from "@mui/material";
+import {Stack, TextField} from "@mui/material";
+import {Dayjs} from "dayjs";
 
 const DateCard = () => {
-  const [date, setDate] = useState(null)
+  const [date, setDate] = useState<Dayjs | null>(null)
+  const [time, setTime] = useState<Dayjs | null>(null)
   const [disabledReset, setDisabledReset] = useState(true)
   const dispatch = useAppDispatch()
 
   const changeDatetimeHandler = () => {
-    if (date) {
+    if (date && time) {
       setDisabledReset(false)
-      console.log(new Date(date).toLocaleString().slice(0, -3))
-      dispatch(chatSlice.actions.setDatetime(new Date(date).toLocaleString().slice(0, -3)))
+      const newDate = new Date(date.toISOString()).toLocaleString()
+      const newTime = new Date(time.toISOString()).toLocaleString()
+      dispatch(chatSlice.actions.setDatetime(newDate.slice(0, -8).concat(newTime.slice(-8))))
     }
   }
 
   const resetDatetimeHandler = () => {
     setDate(null)
+    setTime(null)
     dispatch(chatSlice.actions.setDatetime(''))
     setDisabledReset(true)
   }
 
   return (
-    <Card style={{height: 280, margin: 'auto'}}>
+    <Card style={{height: 400, margin: 'auto', marginRight: 100, marginLeft: 100}}>
       <Card.Header><Card.Title style={{textAlign: 'center'}}>Запрос сообщений с определённой даты</Card.Title></Card.Header>
       <Card.Body>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker
-            renderInput={(props) => <TextField {...props} />}
-            label="Выберите дату"
-            value={date}
-            onChange={(newValue) => {
-              setDate(newValue);
-            }}
-          />
+          <Stack spacing={5}>
+            <DatePicker
+              label="Basic example"
+              value={date}
+              onChange={(newValue) => {
+                setDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <TimePicker
+              ampm={false}
+              openTo="hours"
+              views={['hours', 'minutes', 'seconds']}
+              inputFormat="HH:mm:ss"
+              mask="__:__:__"
+              label="With seconds"
+              value={time}
+              onChange={(newValue) => {
+                setTime(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </Stack>
         </LocalizationProvider>
       </Card.Body>
       <Card.Footer>
